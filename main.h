64a9309163d7de69bc6017d33b13b613716af65b
LC1A_02_asuma <k021g1466@g.neec.ac.jp>
2022-06-21 13:53:45 +0900

    Draw Tirangle

--- main.h
+++ main.h
@@ -1,10 +1,18 @@
-//#pragma once
+#pragma once
 #include "Windows.h"
 #include "d3d12.h"
 #include "dxgi1_6.h"
 #include "cassert"
 #include "vector"
 #include "string"
+#include "DirectXMath.h"
+#include "d3dcompiler.h"
+
+using namespace DirectX;
+
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+#pragma comment(lib,"d3dcompiler.lib")
 
 //ウィンドウサイズ
 const int window_width = 1280;	//横幅
