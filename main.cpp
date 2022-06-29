1ee3fcc45c09dc4ed7cca03bce002222ed0bedb6
LC1A_02_asuma <k021g1466@g.neec.ac.jp>
2022-06-29 11:55:56 +0900

    射影変換

--- main.cpp
+++ main.cpp
@@ -240,10 +240,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 	//頂点データ
 	Vertex vertices[] = {
-		{{	0.0f,100.0f,0.0f},{0.0f,1.0f} },	//左下
-		{{	0.0f,  0.0f,0.0f},{0.0f,0.0f} },	//左上
-		{{100.0f,100.0f,0.0f},{1.0f,1.0f} },	//右下
-		{{100.0f,  0.0f,0.0f},{1.0f,0.0f} },	//右上
+		{{-50.0f,-50.0f,50.0f},{0.0f,1.0f} },	//左下
+		{{-50.0f, 50.0f,50.0f},{0.0f,0.0f} },	//左上
+		{{ 50.0f,-50.0f,50.0f},{1.0f,1.0f} },	//右下
+		{{ 50.0f, 50.0f,50.0f},{1.0f,0.0f} },	//右上
 	};
 	//インデックスデータ
 	unsigned short indices[] =
@@ -762,12 +762,25 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	assert(SUCCEEDED(result));
 
 	//単位行列を代入
-	constMapTransform->mat = XMMatrixIdentity();
+	/*constMapTransform->mat = XMMatrixIdentity();
 	constMapTransform->mat.r[0].m128_f32[0] = 2.0f / window_width;
 	constMapTransform->mat.r[1].m128_f32[1] = -2.0f / window_height;
 	constMapTransform->mat.r[3].m128_f32[0] = -1.0f;
-	constMapTransform->mat.r[3].m128_f32[1] = 1.0f;
+	constMapTransform->mat.r[3].m128_f32[1] = 1.0f;*/
 
+	//平行投影行列の計算
+	/*constMapTransform->mat = XMMatrixOrthographicOffCenterLH(
+		0.0f, window_width,
+		window_height, 0.0f,
+		0.0f, 1.0f
+	);*/
+
+	//透視投影行列の計算
+	constMapTransform->mat = XMMatrixPerspectiveFovLH(
+		XMConvertToRadians(45.0f),			//上下画角45度
+		(float)window_width / window_height,//アスペクト比(画面横幅/画面立幅)
+		0.1f,1000.0f						//前端、奥端
+	);
 
 #pragma endregion
 
